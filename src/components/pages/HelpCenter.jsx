1b08d4920be074f726d2a078f171f9eff1cd4afe
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-04-15 20:33:59 +0100

    Update file

--- src/components/pages/HelpCenter.jsx
+++ src/components/pages/HelpCenter.jsx
@@ -7,13 +7,13 @@ const HelpCenter = () => {
             <h3>How can we help you today?</h3>
             <div className="searchbar">
                 <form action="">
-                    <input type="text" placeholder='Search for help...' id='changeborder' />
+                    <input type="text" placeholder='Search for help...' />
                     <button className="btn">Search</button>
                 </form>
             </div>
             <h3>Interns Help Page</h3>
             <form action="">
-                <textarea name="" id="changeborder1" cols="30" rows="10" placeholder='Info and troubleshooting for posting opportunities'></textarea>
+                <textarea name="" id="" cols="30" rows="10" placeholder='Info and troubleshooting for posting opportunities'></textarea>
             </form>
         </div>
         <h3>Talk to SiliconVerse</h3>
