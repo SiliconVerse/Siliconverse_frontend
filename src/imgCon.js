e5f1c67bf7b0ac3de67ba5865eb1b054662e82ae
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2024-06-22 02:11:08 +0100

    added the og team info

--- src/imgCon.js
+++ src/imgCon.js
@@ -1,16 +1,7 @@
 import hero from "./images/hero.svg";
-import team1 from "./images/team-1.png";
-import team2 from "./images/team-2.png";
-import team3 from "./images/team-3.png";
-import team4 from "./images/team-4.png";
 import goal from "./images/goal.svg";
-
 
 export default {
   hero,
-  team1,
-  team2,
-  team3,
-  team4,
-  goal
+  goal,
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
-export { default as images } from './imgCon'
+export { default as images } from "./imgCon";
+
+import team1 from "./images/team-1.jpeg";
+import team2 from "./images/team-2.jpg";
+import team3 from "./images/team-3.jpeg";
+import team4 from "./images/team-4.jpg";
+import team5 from "./images/team-5.jpeg";
+import team6 from "./images/team-6.jpeg";
+
+export default {
+  team1,
+  team2,
+  team3,
+  team4,
+  team5,
+  team6,
+};
+
+export const teamDetails = [
+  {
+    img: team1,
+    name: "Amaechi Chinonso",
+    title: "Product Lead",
+    linkedinurl: "https://www.linkedin.com/in/amaechinonso",
+    twitterurl: "https://twitter.com/amaechinonso",
+    facebookurl: "",
+    instagramurl: "https://www.instagram.com/amaechinonso_",
+  },
+  {
+    img: team2,
+    name: "Thompson Falowo",
+    title: "Graphics Lead",
+    linkedinurl:
+      "https://www.linkedin.com/in/thompson-falowo-b8b338221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/Falowo_Thompson?t=AXxa8gxVIbaJ7AfUuMJsWg&s=09",
+    facebookurl: "",
+    instagramurl:
+      "https://www.instagram.com/thompson_falowo?igsh=bzV3dXczdW1yZGx",
+  },
+  {
+    img: team3,
+    name: "Francisca N Ogbonna",
+    title: "Lead Project Manager",
+    linkedinurl:
+      "https://www.linkedin.com/in/francisca-n-ogbonna-191721225?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/NgOgbonna",
+    facebookurl: "",
+    instagramurl:
+      "https://www.instagram.com/francisca_ogbonna?igsh=OGQ5ZDc2ODk2ZA==",
+  },
+  {
+    img: team4,
+    name: "Luka Ashini-uka David",
+    title: "Product Design Lead",
+    linkedinurl:
+      "https://www.linkedin.com/in/david-luka-04859a19a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/ashini_uka",
+    facebookurl: "",
+    instagramurl: "https://www.instagram.com/archpopegraphics",
+  },
+  {
+    img: team5,
+    name: "Yomi Abraham Alimi",
+    title: "Community Manager",
+    linkedinurl: "https://www.linkedin.com/in/yomialimi",
+    twitterurl: "",
+    facebookurl: "https://www.facebook.com/yomialimi",
+    instagramurl: "https://www.instagram.com/yomite__/",
+    githuburl: "https://github.com/Alimiyomi",
+  },
+  {
+    img: team6,
+    name: "Michael Amadi",
+    title: "Backend Lead",
+    linkedinurl: "https://www.linkedin.com/in/Mgregchi",
+    twitterurl: "https://x.com/5onOfMan",
+    facebookurl: "",
+    instagramurl: "",
+    githuburl: "https://www.github.com/Mgregchi",
+  },
+];
