50c620951c732a03bf4ea602204df7a5b028dcdd
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-07-13 18:09:33 +0100

    Fix: Build Error Missing CurriculumVitae

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,82 @@
 /* eslint-disable react/jsx-no-undef */
-import Footer from "../src/components/Footer";
-import Navbar from "../src/components/Navbar";
-import Home from "../src/components/pages/Home";
+import Footer from '../src/components/Footer';
+import Navbar from '../src/components/Navbar';
+import Home from '../src/components/pages/Home';
 // import Jobs from "../src/components/Jobs";
-import { Routes, Route } from "react-router-dom";
-import About from "./components/pages/About.jsx";
-import Interns from "./components/pages/Interns.jsx";
-import Mission from "./components/pages/Mission.jsx";
-import HelpCenter from "./components/pages/HelpCenter.jsx";
+import { Routes, Route } from 'react-router-dom';
+import About from './components/pages/About.jsx';
+import Interns from './components/pages/Interns.jsx';
+import Mission from './components/pages/Mission.jsx';
+import HelpCenter from './components/pages/HelpCenter.jsx';
 // import Layout from "./components/layout/layout.jsx";
-import CompanyProfilePage from "./components/pages/CompanyProfilePage.jsx";
-import UserProfile from "./components/user-profile/userProfile.jsx";
-import Magazine from "./components/pages/Magazine.jsx";
+import CompanyProfilePage from './components/pages/CompanyProfilePage.jsx';
+import UserProfile from './components/user-profile/userProfile.jsx';
+import Magazine from './components/pages/Magazine.jsx';
 // Auth imports
-import LoginForm from "./components/pages/login/LogIn.jsx";
-import SignUp from "./components/pages/SignUp.jsx";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Organization from "./components/pages/Organization.jsx";
-import CurriculumVitae from "./components/pages/CurriculumVitae.jsx";
-import PrivacyPolicy from "./components/pages/PrivacyPolicy.jsx";
+import LoginForm from './components/pages/login/LogIn.jsx';
+import SignUp from './components/pages/SignUp.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Organization from './components/pages/Organization.jsx';
+import CurriculumVitae from './components/pages/CurriculumVitae.jsx';
+import PrivacyPolicy from './components/pages/PrivacyPolicy.jsx';
 
 const App = () => {
   return (
-    <div className="w-full">
-      <div className="w-full">
-        <div className="">
+    <div className='w-full'>
+      <div className='w-full'>
+        <div className=''>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/intern" element={<Interns />} />
-            <Route path="/mission" element={<Mission />} />
-            <Route path="/profile" element={<CompanyProfilePage />} />
-            <Route path="/curriculumvitae" element={<CurriculumVitae />} />
-            <Route path="/help" element={<HelpCenter />} />
-            <Route path="/userProfile" element={<UserProfile />} />
-            <Route path="/magazine" element={<Magazine />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/organization" element={<Organization />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/about'
+              element={<About />}
+            />
+            <Route
+              path='/intern'
+              element={<Interns />}
+            />
+            <Route
+              path='/mission'
+              element={<Mission />}
+            />
+            <Route
+              path='/profile'
+              element={<CompanyProfilePage />}
+            />
+            {/* <Route path="/curriculumvitae" element={<CurriculumVitae />} /> */}
+            <Route
+              path='/help'
+              element={<HelpCenter />}
+            />
+            <Route
+              path='/userProfile'
+              element={<UserProfile />}
+            />
+            <Route
+              path='/magazine'
+              element={<Magazine />}
+            />
+            <Route
+              path='/login'
+              element={<LoginForm />}
+            />
+            <Route
+              path='/signup'
+              element={<SignUp />}
+            />
+            <Route
+              path='/organization'
+              element={<Organization />}
+            />
+            <Route
+              path='/privacy'
+              element={<PrivacyPolicy />}
+            />
           </Routes>
           {/* <Home /> */}
         </div>
--- src/components/pages/CurriculumVitae.jsx
+++ src/components/pages/CurriculumVitae.jsx
@@ -1,23 +1,23 @@
-import { Upload } from "lucide-react";
-import cvimg from "../../images/cv_image.png";
+import { Upload } from 'lucide-react';
+import cvimg from '../../images/cv_image.png';
 
 function CurriculumVitae() {
   return (
     <div>
-      <div className="px-6 md:px-48 my-8">
-        <div className="mt-10">
-          <div className="flex flex-col lg:flex-row justify-between items-center gap-10">
+      <div className='px-6 md:px-48 my-8'>
+        <div className='mt-10'>
+          <div className='flex flex-col lg:flex-row justify-between items-center gap-10'>
             <div>
-              <h1 className="text-3xl md:text-5xl font-bold">
+              <h1 className='text-3xl md:text-5xl font-bold'>
                 Curriculum Vitae
               </h1>
-              <p className="text-[13px] md:text-[16px] py-4 text-[#828282]">
+              <p className='text-[13px] md:text-[16px] py-4 text-[#828282]'>
                 Email your C.V to our team for review and pointers
               </p>
-              <p className="text-[13px] md:text-[16px] leading-6">
-                Body text for your whole article or post. We’ll put in some
-                lorem ipsum to show how a filled-out page might look: <br />{" "}
-                <br />
+              <p className='text-[13px] md:text-[16px] leading-6'>
+                Body text for your whole article or post. We&rsquo;ll put in
+                some lorem ipsum to show how a filled-out page might look:{' '}
+                <br /> <br />
                 Excepteur efficient emerging, minim veniam anim aute carefully
                 curated Ginza conversation exquisite perfect nostrud nisi
                 intricate Content. Qui international first-class nulla ut.
@@ -32,17 +32,17 @@ function CurriculumVitae() {
             <div>
               <img
                 src={cvimg}
-                className="w-[90rem] h-full rounded-lg"
-                alt="image"
+                className='w-[90rem] h-full rounded-lg'
+                alt='image'
               />
             </div>
           </div>
 
           <div>
-            <h1 className="text-[15px] md:text-[20px] py-4 text-[#828282] font-semibold">
+            <h1 className='text-[15px] md:text-[20px] py-4 text-[#828282] font-semibold'>
               Why you should have a good C.V
             </h1>
-            <p className="text-[13px] md:text-[16px] leading-6">
+            <p className='text-[13px] md:text-[16px] leading-6'>
               Body text for your whole article or post. We’ll put in some lorem
               ipsum to show how a filled-out page might look: <br />
               <br /> Excepteur efficient emerging, minim veniam anim aute
@@ -57,14 +57,14 @@ function CurriculumVitae() {
             </p>
           </div>
 
-          <div className="my-14">
-            <button className="border border-gray-300 rounded-md p-5 w-[100%] md:w-[40%]">
-              <div className="flex flex-col md:flex-row justify-between items-center gap-5">
-                <div className="flex gap-2 items-center font-bold">
+          <div className='my-14'>
+            <button className='border border-gray-300 rounded-md p-5 w-[100%] md:w-[40%]'>
+              <div className='flex flex-col md:flex-row justify-between items-center gap-5'>
+                <div className='flex gap-2 items-center font-bold'>
                   <Upload size={16} /> <h1>Upload Resume</h1>
                 </div>
                 <div>
-                  <div className="bg-[#FF5F15] text-white p-1 px-5 rounded-lg text-[0.8rem]">
+                  <div className='bg-[#FF5F15] text-white p-1 px-5 rounded-lg text-[0.8rem]'>
                     Share
                   </div>
                 </div>
