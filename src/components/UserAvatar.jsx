c022a1bb859b3d69488fddf62bed0f46f78642f8
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-07-19 14:45:28 +0100

    Update: Updated user Menu

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import profileBigImg from '../assets/profileImgBig.png';
-import { LogOut, User2Icon } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks/userAuth';
+import UserMenu from './UserMenuLinks';
 
 function UserAvatar() {
   const [openUserMenu, setOpenUserMenu] = useState(false);
@@ -42,30 +40,3 @@ function UserAvatar() {
   );
 }
 export default UserAvatar;
-
-function UserMenu({ componentRef }) {
-  const { signout } = useAuth();
-
-  return (
-    <div
-      ref={componentRef}
-      className='absolute top-10 z-20 right-0 w-[200px] bg-white rounded-lg border border-primaryColor p-2'>
-      <Link
-        to={'/user-profile'}
-        className='flex gap-3 hover:bg-primaryColor/90 p-2 rounded-lg text-primaryColor hover:text-white'>
-        <span>
-          <User2Icon size={28} />
-        </span>
-        <span>Profile</span>
-      </Link>
-      <button
-        onClick={() => signout()}
-        className='flex gap-3 hover:bg-primaryColor/90 p-2 rounded-lg text-primaryColor hover:text-white w-full'>
-        <span>
-          <LogOut size={28} />
-        </span>
-        <span>Logout</span>
-      </button>
-    </div>
-  );
-}
---
+++ src/components/UserMenuLinks.jsx
@@ -0,0 +1,32 @@
+import { LogOut, User2Icon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/userAuth';
+
+function UserMenu({ componentRef }) {
+  const { signout, user } = useAuth();
+
+  return (
+    <div
+      ref={componentRef}
+      className='absolute top-10 z-20 right-0 w-[200px] bg-white rounded-lg border border-primaryColor p-2'>
+      <Link
+        to={`/${user.role}-profile`}
+        className='flex gap-3 hover:bg-primaryColor/90 p-2 rounded-lg text-primaryColor hover:text-white'>
+        <span>
+          <User2Icon size={28} />
+        </span>
+        <span>Profile</span>
+      </Link>
+      <button
+        onClick={() => signout()}
+        className='flex gap-3 hover:bg-primaryColor/90 p-2 rounded-lg text-primaryColor hover:text-white w-full'>
+        <span>
+          <LogOut size={28} />
+        </span>
+        <span>Logout</span>
+      </button>
+    </div>
+  );
+}
+
+export default UserMenu;
