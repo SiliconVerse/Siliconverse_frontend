6e971fec44401b70308b0a9477cb80e4cec044b8
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-08-02 16:26:44 +0100

    Update: Team details

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -1,57 +1,78 @@
-import { Facebook, Github, Instagram, Linkedin, Twitter } from 'lucide-react';
-import { teamDetails } from '../../utils/team-details';
-import { Link } from 'react-router-dom';
+import { Facebook, Github, Instagram, Linkedin, Twitter } from "lucide-react";
+import { teamDetails } from "../../utils/team-details";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
 
 function Team() {
   return (
-    <div className='w-full'>
-      <div className='text-center'>
-        <h1 className='text-[1.7rem] font-bold'>Meet Our Team</h1>
+    <div className="w-full">
+      <div className="text-center">
+        <h1 className="text-[1.7rem] font-bold">Meet Our Team</h1>
       </div>
       <div>
         <div>
-          <div className='bg-white'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-center items-start mt-4'>
+          <div className="bg-white">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-center items-start mt-4">
               {teamDetails.map((info) => (
                 <div
                   key={info.name}
-                  className='flex flex-col shadow-xl rounded-lg w-full mx-auto'>
+                  className="flex flex-col shadow-xl rounded-lg w-full mx-auto">
                   <img
                     src={info.img}
-                    className='w-full rounded-t-lg h-[19.3rem]'
-                    alt='images'
+                    className="w-full rounded-t-lg h-[19.3rem]"
+                    alt="images"
                   />
-                  <div className='p-5 py-6'>
-                    <h1 className='text-primaryColor font-semibold text-nowrap'>
+                  <div className="p-5 py-6">
+                    <h1 className="text-primaryColor font-semibold text-nowrap">
                       {info.name}
                     </h1>
-                    <p className='mt-1 text-sm font-normal'>{info.title}</p>
-                    <ul className='flex gap-2 mt-6'>
-                      <li>
-                        <Link to={info.linkedinurl}>
-                          <Linkedin size={17} />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={info.twitterurl}>
-                          <Twitter size={17} />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={info.instagramurl}>
-                          <Instagram size={17} />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={info.githuburl}>
-                          <Github size={17} />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={info.facebookurl}>
-                          <Facebook size={17} />
-                        </Link>
-                      </li>
+                    <p className="mt-1 text-sm font-normal">{info.title}</p>
+                    <ul className="flex gap-2 mt-6">
+                      {info.linkedinurl && (
+                        <li>
+                          <Link to={info.linkedinurl}>
+                            <FaLinkedin
+                              size={24}
+                              className="text-blue-500"
+                            />
+                          </Link>
+                        </li>
+                      )}
+                      {info.twitterurl && (
+                        <li>
+                          <Link to={info.twitterurl}>
+                            <FaX size={24} />
+                          </Link>
+                        </li>
+                      )}
+                      {info.instagramurl && (
+                        <li>
+                          <Link to={info.instagramurl}>
+                            <Instagram size={24} />
+                          </Link>
+                        </li>
+                      )}
+                      {info.githuburl && (
+                        <li>
+                          <Link to={info.githuburl}>
+                            <FaGithub
+                              size={24}
+                              className="text-gray-900"
+                            />
+                          </Link>
+                        </li>
+                      )}
+                      {info.facebookurl && (
+                        <li>
+                          <Link to={info.facebookurl}>
+                            <FaFacebook
+                              size={24}
+                              className="text-blue-800"
+                            />
+                          </Link>
+                        </li>
+                      )}
                     </ul>
                   </div>
                 </div>
--- src/utils/team-details.js
+++ src/utils/team-details.js
@@ -1,9 +1,10 @@
-import team1 from '../assets/team-1.jpeg';
-import team2 from '../assets/team-2.jpg';
-import team3 from '../assets/team-3.jpeg';
-import team4 from '../assets/team-4.jpg';
-import team5 from '../assets/team-5.jpeg';
-import team6 from '../assets/team-6.jpeg';
+import team1 from "../assets/team-1.jpeg";
+import team2 from "../assets/team-2.jpg";
+import team3 from "../assets/team-3.jpeg";
+import team4 from "../assets/team-4.jpg";
+import team5 from "../assets/team-5.jpeg";
+import team6 from "../assets/team-6.jpeg";
+import vincent from "../assets/vincent.jpg";
 
 export default {
   team1,
@@ -12,68 +13,79 @@ export default {
   team4,
   team5,
   team6,
+  vincent,
 };
 
 export const teamDetails = [
   {
     img: team1,
-    name: 'Amaechi Chinonso',
-    title: 'Product Lead',
-    linkedinurl: 'https://www.linkedin.com/in/amaechinonso',
-    twitterurl: 'https://twitter.com/amaechinonso',
-    facebookurl: '',
-    instagramurl: 'https://www.instagram.com/amaechinonso_',
+    name: "Amaechi Chinonso",
+    title: "Product Lead",
+    linkedinurl: "https://www.linkedin.com/in/amaechinonso",
+    twitterurl: "https://twitter.com/amaechinonso",
+    facebookurl: "",
+    instagramurl: "https://www.instagram.com/amaechinonso_",
   },
   {
     img: team2,
-    name: 'Thompson Falowo',
-    title: 'Graphics Lead',
+    name: "Thompson Falowo",
+    title: "Graphics Lead",
     linkedinurl:
-      'https://www.linkedin.com/in/thompson-falowo-b8b338221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-    twitterurl: 'https://x.com/Falowo_Thompson?t=AXxa8gxVIbaJ7AfUuMJsWg&s=09',
-    facebookurl: '',
+      "https://www.linkedin.com/in/thompson-falowo-b8b338221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/Falowo_Thompson?t=AXxa8gxVIbaJ7AfUuMJsWg&s=09",
+    facebookurl: "",
     instagramurl:
-      'https://www.instagram.com/thompson_falowo?igsh=bzV3dXczdW1yZGx',
+      "https://www.instagram.com/thompson_falowo?igsh=bzV3dXczdW1yZGx",
   },
   {
     img: team3,
-    name: 'Francisca N Ogbonna',
-    title: 'Lead Project Manager',
+    name: "Francisca N Ogbonna",
+    title: "Lead Project Manager",
     linkedinurl:
-      'https://www.linkedin.com/in/francisca-n-ogbonna-191721225?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-    twitterurl: 'https://x.com/NgOgbonna',
-    facebookurl: '',
+      "https://www.linkedin.com/in/francisca-n-ogbonna-191721225?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/NgOgbonna",
+    facebookurl: "",
     instagramurl:
-      'https://www.instagram.com/francisca_ogbonna?igsh=OGQ5ZDc2ODk2ZA==',
+      "https://www.instagram.com/francisca_ogbonna?igsh=OGQ5ZDc2ODk2ZA==",
   },
   {
     img: team4,
-    name: 'Luka Ashini-uka David',
-    title: 'Product Design Lead',
+    name: "Luka Ashini-uka David",
+    title: "Product Design Lead",
     linkedinurl:
-      'https://www.linkedin.com/in/david-luka-04859a19a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
-    twitterurl: 'https://x.com/ashini_uka',
-    facebookurl: '',
-    instagramurl: 'https://www.instagram.com/archpopegraphics',
+      "https://www.linkedin.com/in/david-luka-04859a19a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitterurl: "https://x.com/ashini_uka",
+    facebookurl: "",
+    instagramurl: "https://www.instagram.com/archpopegraphics",
   },
   {
     img: team5,
-    name: 'Yomi Abraham Alimi',
-    title: 'Community Manager',
-    linkedinurl: 'https://www.linkedin.com/in/yomialimi',
-    twitterurl: '',
-    facebookurl: 'https://www.facebook.com/yomialimi',
-    instagramurl: 'https://www.instagram.com/yomite__/',
-    githuburl: 'https://github.com/Alimiyomi',
+    name: "Yomi Abraham Alimi",
+    title: "Community Manager",
+    linkedinurl: "https://www.linkedin.com/in/yomialimi",
+    twitterurl: "",
+    facebookurl: "https://www.facebook.com/yomialimi",
+    instagramurl: "https://www.instagram.com/yomite__/",
+    githuburl: "https://github.com/Alimiyomi",
   },
   {
     img: team6,
-    name: 'Michael Amadi',
-    title: 'Backend Lead',
-    linkedinurl: 'https://www.linkedin.com/in/Mgregchi',
-    twitterurl: 'https://x.com/5onOfMan',
-    facebookurl: '',
-    instagramurl: '',
-    githuburl: 'https://www.github.com/Mgregchi',
+    name: "Michael Amadi",
+    title: "Backend Lead",
+    linkedinurl: "https://www.linkedin.com/in/Mgregchi",
+    twitterurl: "https://x.com/5onOfMan",
+    facebookurl: "",
+    instagramurl: "",
+    githuburl: "https://www.github.com/Mgregchi",
+  },
+  {
+    img: vincent,
+    name: "Olaoluwa Vincent",
+    title: "Frontend Lead",
+    linkedinurl: "https://www.linkedin.com/in/olaoluwavincent",
+    twitterurl: "https://x.com/olaoluwavincen5",
+    facebookurl: "",
+    instagramurl: "",
+    githuburl: "https://www.github.com/olaoluwavincent",
   },
 ];
