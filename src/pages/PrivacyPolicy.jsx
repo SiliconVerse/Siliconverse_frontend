0d790e09688d1fec2f8bbca015e0d9b0b9c5a760
oyekunle-coder <oyekunleshobowale@gmail.com>
2024-10-14 10:18:50 +0100

    Fixed the policy error

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -12,14 +12,11 @@ const PrivacyPolicy = () => {
         </p> */}
 
         <p>
-            Siliconverse retains the right to amend, modify, add, 
-            or remove sections of this Privacy Policy at any time and 
-            without prior notice. Any alterations will take effect 
-            immediately upon being posted, unless stated otherwise. 
-            We advise you to periodically review this Privacy Policy to 
-            stay informed about any updates or modifications. Your continued 
-            use of the Platform after amendments to this Privacy 
-            Policy indicates your continued acceptance of its terms and conditions.
+          At Siliconverse, we are dedicated to protecting the privacy of our users and safeguarding the information they share with us. Our commitment to data protection is embedded throughout our Platform and Services, ensuring effective privacy measures are in place.
+          By accessing or using the Siliconverse website, online services, mobile applications, and social networking platforms (collectively referred to as the "Platform"), you agree to the practices and policies detailed in this Privacy Policy and our Terms and Conditions. Each time you use the Platform, you consent to the collection, use, and disclosure of your information as outlined in this policy.
+          The terms "we," "us," "our," and "Siliconverse" refer to Siliconverse and its affiliates. "Users," "you," and "your" refer to both talents and businesses as defined in our Terms and Conditions.
+          Siliconverse is committed to maintaining transparency and accountability in our data handling practices. We continuously strive to protect your data and ensure that your privacy is respected and upheld across all our services. Your trust is important to us, and we take every measure to ensure your information is secure and used responsibly.
+
         </p>
       </div>
       <div className='pt-20 space-y-10'>
@@ -31,16 +28,16 @@ const PrivacyPolicy = () => {
           </p> */}
 
           <p>
-          A polished CV is your ticket to career success. It showcases 
-          your skills, experience, and achievements, leaving a 
-          lasting impression on potential employers. 
-          With a well-crafted CV, you'll stand out from the competition, 
-          secure interviews, and open doors to exciting 
-          job opportunities. Invest in your future with a stellar CV.
+            Siliconverse retains the right to amend, modify, add, 
+            or remove sections of this Privacy Policy at any time and 
+            without prior notice. Any alterations will take effect 
+            immediately upon being posted, unless stated otherwise. 
+            We advise you to periodically review this Privacy Policy to 
+            stay informed about any updates or modifications. Your continued 
+            use of the Platform after amendments to this Privacy 
+            Policy indicates your continued acceptance of its terms and conditions..
           </p>
         </div>
-
-        <UploadResumeInput />
       </div>
     </div>
   );
