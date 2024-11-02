64f620acf31ba554a0c79cd8b0af67e449fe83e8
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-11-02 14:31:45 +0100

    fixed spelling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Poppins', 'sans-serif'],
+        roboto: ["Poppins", "sans-serif"],
       },
       colors: {
-        primaryColor: '#FF5F15',
-        'silicon-green': '#2EB67D',
-        'silcon-red': '#FF0000',
-        white: '#FFFFFF',
-        'text-color': '#222222',
-        'silicon-gray': ' #2F2E2E',
-        'background-color': '#FDEFE9',
-        'off-white': '#D9D9D9',
+        primaryColor: "#FF5F15",
+        "silicon-green": "#2EB67D",
+        "silicon-red": "#FF0000",
+        white: "#FFFFFF",
+        "text-color": "#222222",
+        "silicon-gray": " #2F2E2E",
+        "background-color": "#FDEFE9",
+        "off-white": "#D9D9D9",
         gray: {
-          700: '#A3A3A3',
+          700: "#A3A3A3",
         },
       },
 
       borderRadius: {
-        sl: '10px',
+        sl: "10px",
       },
     },
   },
