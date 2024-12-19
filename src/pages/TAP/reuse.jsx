6acecebfe52690ba628838eb3592e649dedac044
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-12-19 17:15:08 +0100

    (feat): TAP hero section complete for mobile view

--- src/pages/TAP/reuse.jsx
+++ src/pages/TAP/reuse.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
 
-export function HeroImage({ bgImage, fgImage, alt }) {
+export function HeroImage({ bgImage, fgImage, alt, className }) {
     return (
         <div
-            className="mx-auto bg-no-repeat bg-cover "
+            className={`mx-auto bg-no-repeat bg-cover ${className} `}
             style={{ backgroundImage: `url(${bgImage})` }}>
-            <img src={fgImage} alt={alt} />
+            <img src={fgImage} alt={alt} className='h-[100%] w-[100%] aspect-auto' />
         </div>
     )
 }
