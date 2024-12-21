04ac4f5a6b136592b0fffe93fe395a7058b5cf48
heart <150511179+Nuelsti@users.noreply.github.com>
2024-12-21 19:42:23 +0100

    worked on the PR error

--- src/pages/Privacy/Info1/index.jsx
+++ src/pages/Privacy/Info1/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.css'
+import Infotwo from '../Info2'
 
 function InfoOne() {
     return (
@@ -11,7 +12,7 @@ function InfoOne() {
             </p>
             <ol className="info_one_list">
                 <li>
-                    <b>1.1</b> <span className="head_info1" style={{color: ''}}> Personal Information: </span>
+                    <b>1.1</b> <span className="head_info1"> Personal Information: </span>
                     <p className="info-one-list-cont">
                         Personal information refers to any data that can be used to identify you as an individual. This information is collected directly from you when you interact with our website or services. 
                         Personal information is essential for providing tailored services and relevant content. 
@@ -42,8 +43,36 @@ function InfoOne() {
                         </li>
                     </ul>
                 </li>
-                <li></li>
+                {/* <li></li> */}
+
+                <li>
+                    <b>1.2</b> <span className="head_info1">Non-Personal Information: </span>
+                    <p className="info-one-list-cont">
+                        Non-personal information refers to data that cannot directly identify you as an individual. This type of information is collected automatically when 
+                        you interact with our website, and it helps us to improve the overall functionality and performance
+                        of Siliconmagazine.                     
+                    </p>
+                    <p className="info-two-list-cont-ul">Non-personal information includes:</p>
+                    <ul className="info-one-list-cont-ul">
+                        <li>
+                        IP Address: This helps us understand your general location, identify unusual access patterns, and improve security. Additionally, knowing the region from which you're visiting enables us to recommend region-specific content.                        </li>
+                        <li>
+                        Browser Type and Version: Knowing what browser you're using helps us ensure compatibility with our site’s features and improve your overall experience.
+                        </li>
+                        <li>
+                            Phone Number (Optional):This is used when direct communication is required, 
+                            especially for clarifications on premium services, or for any urgent matters regarding your account.
+                        </li>
+                        <li>
+                        Device Information (e.g., Operating System): This data assists in optimizing the user experience across different devices and platforms, ensuring that you have a smooth interaction whether you're accessing our site via desktop, tablet, or mobile.
+                        </li>
+                        <li>
+                        Cookie Data: Cookies help us provide you with a more personalized experience, storing your preferences so that you don't have to reset them every time you visit. Cookies also allow us to remember you between visits and deliver more relevant ads.
+                        </li>
+                    </ul>
+                </li>
             </ol>
+            {/* <Infotwo /> */}
 
         </div>
         
--- src/pages/Privacy/index.jsx
+++ src/pages/Privacy/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import PrivacyHead from './Header'
 import InfoOne from './Info1'
+import Infotwo from './Info2'
+// import Infotwo from './Info2'
+
 
 function Privacy() {
     return (
         <>
             <PrivacyHead />
             <InfoOne />
+            {/* <Infotwo /> */}
+            <Infotwo />
         </>
         
     )
