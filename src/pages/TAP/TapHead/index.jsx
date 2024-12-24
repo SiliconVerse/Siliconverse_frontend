709218d189505f0fc1c15512a8ff415ff5d58e45
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-12-24 10:59:12 +0100

    (feat): TAPHEAD desktop view

--- src/pages/TAP/TapHead/index.jsx
+++ src/pages/TAP/TapHead/index.jsx
@@ -5,27 +5,26 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { HeroImage, ApplyButton, applyForm } from "../reuse";
 
-
 function Taphead() {
 	useEffect(() => {
 		AOS.init({ duration: 1000 }); // Initialize AOS
 	}, []);
 	return (
-		<section className="bg-[#FF5F15] px-4 py-6 text-white flex flex-col space-y-20">
+		<section className="bg-[#FF5F15] px-4 py-6 text-white space-y-20">
 			<div className="flex flex-col items-center space-y-12">
-				<div className="grid items-center grid-cols-1 text-center divide-y-2 divide-gray-100 gap-y-8">
-					<div className="grid items-center gap-y-6 grid-cols-1 sm:grid-cols-2 w-[100%]">
-						<HeroImage
-							className="h-80"
-							bgImage={BgImage}
-							alt={"person"}
-							fgImage={PersonImg}
-						/>
-						<div  className="w-[100%] flex flex-col space-y-2">
-							<h1 className="text-4xl font-semibold text-center justify-evenly sm:text-6xl">
+				<div className="grid items-center justify-start grid-cols-1 text-start sm:grid-cols-2 gap-y-8">
+					<HeroImage
+						className="h-80 sm:h-96"
+						bgImage={BgImage}
+						alt={"person"}
+						fgImage={PersonImg}
+					/>
+					<div className="sm:order-first grid items-center gap-y-6 grid-cols-1 divide-y-2 divide-gray-100 w-[100%]">
+						<div className="w-[100%] flex flex-col space-y-12">
+							<h1 className="text-4xl font-semibold text-center sm:text-left justify-evenly ">
 								Talent Accelerator Programme (TAP)
 							</h1>
-							<p className="text-sm font-semibold leading-10 sm:text-2xl">
+							<p className="text-sm font-semibold leading-10 sm:text-base">
 								The Talent Accelerator Program (TAP) is an
 								innovative internship initiative by Siliconverse
 								designed to equip emerging tech talents with the
@@ -33,20 +32,19 @@ function Taphead() {
 								in the ever-evolving tech industry.
 							</p>
 						</div>
-						{/* img */}
+						<p className="text-sm font-thin leading-10 sm:text-base">
+							{/* <h2 className="sub__text2"> */}
+							TAP provides an immersive learning environment where
+							talents can work on real-world projects, collaborate
+							with Siliconverse team, and fast-track their career
+							growth.
+						</p>{" "}
 					</div>
-					<p className="text-sm font-normal leading-10 sm:text-2xl">
-						{/* <h2 className="sub__text2"> */}
-						TAP provides an immersive learning environment where
-						talents can work on real-world projects, collaborate
-						with Siliconverse team, and fast-track their career
-						growth.
-					</p>
 				</div>
 				{/* button */}
 				<ApplyButton
 					className={
-						"text-[#fff] bg-transparent border-[1.5px] border-[#FFFF] font-semibold text-lg w-full sm:w-[30%] p-2 rounded-full text-center"
+						"text-[#fff] sm:bg-white sm:text-[#FF5F15] bg-transparent border-[1.5px] border-[#FFFF] font-semibold text-lg w-full sm:w-[30%] p-2 rounded-full text-center"
 					}
 					href={applyForm}>
 					Apply Now!
