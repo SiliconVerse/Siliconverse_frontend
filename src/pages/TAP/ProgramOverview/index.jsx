c582be9a56f69086784ec3dd039547e25df25746
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2024-12-24 11:54:34 +0100

    minor

--- src/pages/TAP/ProgramOverview/index.jsx
+++ src/pages/TAP/ProgramOverview/index.jsx
@@ -21,22 +21,25 @@ function ProgrammOverview() {
 			<h1 className="text-2xl font-bold text-[#FF5F15]">
 				Program Overview
 			</h1>
-			<SectionImage src={Pic} />
-			<div className="flex flex-col space-y-4 ">
-				<p className="">
-					The Talent Accelerator Program (TAP) by Siliconverse is a
-					specialized, hands-on internship that bridges the gap
-					between education and real-world experience. Designed for
-					aspiring tech professionals, TAP offers a unique opportunity
-					to help techies gain experience, collaborate on cutting-edge
-					projects and develop market-ready skills to become
-					employable.
-				</p>
-				<p className="">
-					The goal is simple: To accelerate your learning, enhance
-					your resume, portfolio, and prepare you for a successful
-					career in the tech industry.
-				</p>
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+				<SectionImage src={Pic} />
+
+				<div className="flex flex-col space-y-4 ">
+					<p className="">
+						The Talent Accelerator Program (TAP) by Siliconverse is
+						a specialized, hands-on internship that bridges the gap
+						between education and real-world experience. Designed
+						for aspiring tech professionals, TAP offers a unique
+						opportunity to help techies gain experience, collaborate
+						on cutting-edge projects and develop market-ready skills
+						to become employable.
+					</p>
+					<p className="">
+						The goal is simple: To accelerate your learning, enhance
+						your resume, portfolio, and prepare you for a successful
+						career in the tech industry.
+					</p>
+				</div>
 			</div>
 		</section>
 	);
